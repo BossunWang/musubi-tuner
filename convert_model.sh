bd83e067e25e96ae6fe8176c23f90b7b31f67e72
bossun <vvmodouco@gmail.com>
2025-10-13 09:12:59 +0800

    feat: update model conversion and training scripts for avatar2_wan2.2

--- convert_model.sh
+++ convert_model.sh
@@ -1,2 +1,5 @@
+#python src/musubi_tuner/convert_lora.py \
+#--input output/avatar1_wan2.2_v1-000290.safetensors --output output/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
+
 python src/musubi_tuner/convert_lora.py \
---input output/avatar1_wan2.2_v1-000290.safetensors --output output/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
+--input output_avatar2/avatar2_wan2.2_v1-000290.safetensors --output output_avatar2/convert_lora/avatar2_wan2.2_low_v1.safetensors --target other
