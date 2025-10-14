f22dacd1f4c0941c2aceb99a98ad5cb1a271b6d4
bossun <vvmodouco@gmail.com>
2025-10-14 09:14:12 +0800

    feat: update dataset configuration and training scripts for avatar1_wan2.2

--- convert_model.sh
+++ convert_model.sh
@@ -1,5 +1,5 @@
-#python src/musubi_tuner/convert_lora.py \
-#--input output/avatar1_wan2.2_v1-000290.safetensors --output output/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
-
 python src/musubi_tuner/convert_lora.py \
---input output_avatar2/avatar2_wan2.2_v1-000290.safetensors --output output_avatar2/convert_lora/avatar2_wan2.2_low_v1.safetensors --target other
+--input output_avatar1/avatar1_wan2.2_v1-000200.safetensors --output output_avatar1/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
+
+#python src/musubi_tuner/convert_lora.py \
+#--input output_avatar2/avatar2_wan2.2_v1-000290.safetensors --output output_avatar2/convert_lora/avatar2_wan2.2_low_v1.safetensors --target other
--- run_dataset_enc.sh
+++ run_dataset_enc.sh
@@ -1,5 +1,5 @@
-#python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/avatar1_wan2.2.toml \
-#--vae models/wan2.2/vae/wan_2.1_vae.safetensors
-
-python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/avatar2_wan2.2.toml \
+python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/avatar1_wan2.2.toml \
 --vae models/wan2.2/vae/wan_2.1_vae.safetensors
+
+#python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/avatar2_wan2.2.toml \
+#--vae models/wan2.2/vae/wan_2.1_vae.safetensors
