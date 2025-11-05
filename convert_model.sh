1a158734cd7a904d8f1e690085e4554fefb3dbb5
bossun <vvmodouco@gmail.com>
2025-11-05 10:36:08 +0800

    feat: add configuration and scripts for avatar3_wan2.2 dataset processing and training

--- convert_model.sh
+++ convert_model.sh
@@ -1,5 +1,8 @@
-python src/musubi_tuner/convert_lora.py \
---input output_avatar1/avatar1_wan2.2_v1-000200.safetensors --output output_avatar1/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
+#python src/musubi_tuner/convert_lora.py \
+#--input output_avatar1/avatar1_wan2.2_v1-000200.safetensors --output output_avatar1/convert_lora/avatar1_wan2.2_low_v1.safetensors --target other
 
 #python src/musubi_tuner/convert_lora.py \
 #--input output_avatar2/avatar2_wan2.2_v1-000290.safetensors --output output_avatar2/convert_lora/avatar2_wan2.2_low_v1.safetensors --target other
+
+python src/musubi_tuner/convert_lora.py \
+--input output_avatar3/avatar3_wan2.2_v1-000300.safetensors --output output_avatar3/convert_lora/avatar3_wan2.2_low_v1.safetensors --target other
--- run_text_enc.sh
+++ run_text_enc.sh
@@ -1,5 +1,8 @@
-python src/musubi_tuner/wan_cache_text_encoder_outputs.py --dataset_config dataset/avatar1_wan2.2.toml \
---t5 models/wan2.2/text_encoders/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 16
+#python src/musubi_tuner/wan_cache_text_encoder_outputs.py --dataset_config dataset/avatar1_wan2.2.toml \
+#--t5 models/wan2.2/text_encoders/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 16
 
 #python src/musubi_tuner/wan_cache_text_encoder_outputs.py --dataset_config dataset/avatar2_wan2.2.toml \
 #--t5 models/wan2.2/text_encoders/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 16
+
+python src/musubi_tuner/wan_cache_text_encoder_outputs.py --dataset_config dataset/avatar3_wan2.2.toml \
+--t5 models/wan2.2/text_encoders/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 16
