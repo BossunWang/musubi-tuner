4874493b0cd9286da8386ffb6f5e0aa56251ba31
bossun <vvmodouco@gmail.com>
2025-12-15 17:38:26 +0800

    feat: add scripts and configuration for twice_onespark dataset processing and training

--- convert_model.sh
+++ convert_model.sh
@@ -4,5 +4,8 @@
 #python src/musubi_tuner/convert_lora.py \
 #--input output_avatar2/avatar2_wan2.2_v1-000290.safetensors --output output_avatar2/convert_lora/avatar2_wan2.2_low_v1.safetensors --target other
 
+#python src/musubi_tuner/convert_lora.py \
+#--input output_avatar3/avatar3_wan2.2_v1-000300.safetensors --output output_avatar3/convert_lora/avatar3_wan2.2_low_v1.safetensors --target other
+
 python src/musubi_tuner/convert_lora.py \
---input output_avatar3/avatar3_wan2.2_v1-000300.safetensors --output output_avatar3/convert_lora/avatar3_wan2.2_low_v1.safetensors --target other
+--input output_twice_onespark/twice_onespark_wan2.2_high_v1-000300.safetensors --output output_twice_onespark/convert_lora/twice_onespark_wan2.2_high_v1.safetensors --target other
--- train_low_noise_model.sh
+++ train_low_noise_model.sh
@@ -24,15 +24,28 @@
 #  --min_timestep 0 --max_timestep 875 \
 #  --preserve_distribution_shape
 
+#accelerate launch --num_cpu_threads_per_process 1 --mixed_precision fp16 src/musubi_tuner/wan_train_network.py \
+#  --task t2v-A14B \
+#  --dit models/wan2.2/diffusion_models/wan2.2_t2v_low_noise_14B_fp16.safetensors \
+#  --dataset_config dataset/avatar3_wan2.2.toml --sdpa --mixed_precision fp16 --fp8_base \
+#  --optimizer_type adamw8bit --learning_rate 2e-4 --gradient_checkpointing \
+#  --max_data_loader_n_workers 2 --persistent_data_loader_workers \
+#  --network_module networks.lora_wan --network_dim 32 \
+#  --timestep_sampling shift --discrete_flow_shift 8.0 \
+#  --max_train_epochs 310 --save_every_n_epochs 50 --seed 55 \
+#  --output_dir output_avatar3 --output_name avatar3_wan2.2_v1 --blocks_to_swap 20 \
+#  --min_timestep 0 --max_timestep 875 \
+#  --preserve_distribution_shape
+
 accelerate launch --num_cpu_threads_per_process 1 --mixed_precision fp16 src/musubi_tuner/wan_train_network.py \
-  --task t2v-A14B \
-  --dit models/wan2.2/diffusion_models/wan2.2_t2v_low_noise_14B_fp16.safetensors \
-  --dataset_config dataset/avatar3_wan2.2.toml --sdpa --mixed_precision fp16 --fp8_base \
-  --optimizer_type adamw8bit --learning_rate 2e-4 --gradient_checkpointing \
+  --task i2v-A14B \
+  --dit models/wan2.2/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors \
+  --dataset_config dataset/twice_onespark_wan2.2.toml --sdpa --mixed_precision fp16 --fp8_base \
+  --optimizer_type adamw8bit --learning_rate 1e-4 --gradient_checkpointing \
   --max_data_loader_n_workers 2 --persistent_data_loader_workers \
   --network_module networks.lora_wan --network_dim 32 \
   --timestep_sampling shift --discrete_flow_shift 8.0 \
   --max_train_epochs 310 --save_every_n_epochs 50 --seed 55 \
-  --output_dir output_avatar3 --output_name avatar3_wan2.2_v1 --blocks_to_swap 20 \
+  --output_dir output_twice_onespark --output_name twice_onespark_wan2.2_low_v1 --blocks_to_swap 30 \
   --min_timestep 0 --max_timestep 875 \
   --preserve_distribution_shape
