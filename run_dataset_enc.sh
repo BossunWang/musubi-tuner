80dfe65c34602c8e4c439168da1fe100d6fa9b56
bossun <vvmodouco@gmail.com>
2025-12-26 09:36:50 +0800

    feat: add configuration and scripts for louiswain_wan2.2 dataset processing and training

--- run_dataset_enc.sh
+++ run_dataset_enc.sh
@@ -7,5 +7,8 @@
 #python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/avatar3_wan2.2.toml \
 #--vae models/wan2.2/vae/wan_2.1_vae.safetensors
 
-python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/twice_onespark_wan2.2.toml \
+#python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/twice_onespark_wan2.2.toml \
+#--vae models/wan2.2/vae/wan_2.1_vae.safetensors --clip models/wan2.2/clip/models_clip_open-clip-xlm-roberta-large-vit-huge-14.pth
+
+python src/musubi_tuner/wan_cache_latents.py --dataset_config dataset/louiswain_wan2.2.toml \
 --vae models/wan2.2/vae/wan_2.1_vae.safetensors --clip models/wan2.2/clip/models_clip_open-clip-xlm-roberta-large-vit-huge-14.pth
